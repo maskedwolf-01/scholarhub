0864ff03e7d844238986312dfa6eb9d634525d65
maskedwolf-01 <155622258+maskedwolf-01@users.noreply.github.com>
2025-11-09 08:12:38 +0100

    Add GitHub login button to signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -9,3 +9,18 @@ export default function Signup() {
     </div>
   );
 }
+import { signIn } from "next-auth/react";
+
+export default function Login() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Login Page</h1>
+      <button
+        onClick={() => signIn("github")}
+        style={{ padding: "10px 20px", marginTop: "20px", cursor: "pointer" }}
+      >
+        Login with GitHub
+      </button>
+    </div>
+  );
+}
