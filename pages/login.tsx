615fbcc8e0882d45286bc459dd367bd60e0641e7
maskedwolf-01 <155622258+maskedwolf-01@users.noreply.github.com>
2025-11-09 07:56:03 +0100

    Rename Login component to Home and update content

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,11 @@
-export default function Login() {
-  return <h1>Login Page (placeholder)</h1>;
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to ScholarHub</h1>
+      <p>FUOYE Faculty of Computing Portal</p>
+      <p>
+        <a href="/login">Login</a> | <a href="/signup">Signup</a>
+      </p>
+    </div>
+  );
 }
