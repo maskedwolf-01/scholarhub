110ca83652c3036c99e2178b37f449498e873299
maskedwolf-01 <155622258+maskedwolf-01@users.noreply.github.com>
2025-11-09 08:04:06 +0100

    Add Footer component with copyright information

---
+++ pages/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', marginTop: '50px' }}>
+      <p>© 2025 ScholarHub | FUOYE Faculty of Computing | Developed by Majeed Abdulwali</p>
+    </footer>
+  );
+}
